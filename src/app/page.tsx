fa31e2151b782bbf1c47895ba0f562ddb7f5452f
Tim Day <tim@rome2rio.com>
2025-09-01 02:04:20 +1000

    demo cloudinary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CloudinaryImage from "~/components/CloudinaryImage";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -5,6 +7,8 @@ export default function HomePage() {
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Coin <span className="text-[hsl(280,100%,70%)]">Cabinet</span>
         </h1>
+
+        <CloudinaryImage />
       </div>
     </main>
   );
