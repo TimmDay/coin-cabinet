25a93568a3af4faf6b1c2bc219d40497a06f1a78
Tim Day <tim@rome2rio.com>
2025-09-22 03:30:16 +1000

    tidy: remove unneeded type assertion

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { env } from "~/env";
 
-// TODO: do we need the casting below?
 export const supabase = createClient(
-  env.NEXT_PUBLIC_SUPABASE_URL as string,
-  env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  env.NEXT_PUBLIC_SUPABASE_URL,
+  env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
